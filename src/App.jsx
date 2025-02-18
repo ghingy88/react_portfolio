77aa9651f99ced8a7ff93e238d068bdb3f27b272
s3874111 <s3874111@student.rmit.edu.au>
2025-02-19 00:05:57 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+  <Navbar />
+   </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Menu } from 'lucide-react';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+    <nav className="bg-white shadow">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-16 items-center">
+          <NavLink to="/" className="font-bold text-xl text-gray-800">Portfolio</NavLink>
+          <div className="hidden md:flex space-x-8">
+            <NavLink to="/" className={({ isActive }) => isActive ? 'text-blue-600' : 'text-gray-600 hover:text-blue-600'}>Home</NavLink>
+            </div>
+    </div>
+        </div>
+        </nav>
+
+    
+    );
+  };
+
+//   const Navbar = () => {
+//     return (
+//         <h1 className='text-red-500'>Hi</h1>
+//     );
+//   };
+  
+  export default Navbar;
