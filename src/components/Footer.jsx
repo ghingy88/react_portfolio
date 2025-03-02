d02ebbf9d84dbd5dd50d640bf7f13caab02ec7c6
ghingy88 <ghingy88@gmail.com>
2025-03-03 00:59:56 +1100

    changes to home screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-<footer className="bg-gray-800 text-white py-8">
+<footer className="text-white py-8">
       <div className="container mx-auto px-4">
         <div className="grid md:grid-cols-2 gap-8">
           <div className="col-span-2">
