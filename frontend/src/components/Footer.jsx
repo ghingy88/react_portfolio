cc5992a307f9c83ce05bb013bede5d5dc89a8b0e
ghingy88 <ghingy88@gmail.com>
2025-03-20 19:13:11 +1100

    footer mobile view

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,15 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
-<footer className="text-white py-8">
+    <footer className="text-white py-8">
       <div className="container mx-auto px-4">
-        <div className="grid md:grid-cols-2 gap-8">
-          <div className="col-span-2">
-            
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h2 className="text-xl font-bold mb-4">Quick Links</h2>
-            <ul className="flex space-x-4">
+            <ul className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:space-x-4">
               <li><NavLink to="/" className="text-gray-400 hover:text-white">Home</NavLink></li>
               <li><NavLink to="/About" className="text-gray-400 hover:text-white">About</NavLink></li>
               <li><NavLink to="/Projects" className="text-gray-400 hover:text-white">Projects</NavLink></li>
@@ -21,23 +18,18 @@ const Footer = () => {
           </div>
           <div>
             <h2 className="text-xl font-bold mb-4">Contact Me</h2>
-            <ul className="flex space-x-4">
+            <ul className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:space-x-4">
               <li><a href="https://www.linkedin.com/in/ghingmoluk88/" className="text-gray-400 hover:text-white">LinkedIn</a></li>
               <li><a href="https://github.com/ghingy88" className="text-gray-400 hover:text-white">GitHub</a></li>
             </ul>
           </div>
         </div>
         <div className="mt-8 text-center text-gray-400">
-          &copy; {new Date().getFullYear()} Ghingmo Luk All rights reserved.
+          &copy; {new Date().getFullYear()} Ghingmo Luk. All rights reserved.
         </div>
       </div>
     </footer>
   );
 };
-// const Footer = () => {
-//     return (
-//         <h1>hi</h1>
-//     );
-// };
 
 export default Footer;
